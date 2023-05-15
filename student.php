304f9e0e46d6f665b666987810b2df2d8240c6e8
xbukris <xbukris@stuba.sk>
2023-05-15 13:11:55 +0200

    xbukris(student and teacher home page created student now can use assignment generation)

--- student.php
+++ student.php
@@ -1,2 +1,23 @@
 <?php
-echo "Hello student";
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true && $_SESSION["type"] == "student"){
+    echo "Hello student";
+}
+else{
+    header("location: login.php");
+}
+?>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student</title>
+</head>
+<body>
+<h1>Student</h1>
+<a href="logout.php">logout</a>
+<br>
+<a href="test/asd.php">generate test</a>
+</body>
--- teacher.php
+++ teacher.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello teacher";
+
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true && $_SESSION["type"] == "teacher"){
+    echo "Hello teacher";
+}
+else{
+    header("location: login.php");
+}
+
