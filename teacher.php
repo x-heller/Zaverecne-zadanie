9b9f80ee7e6a10b919f73b90ecbf607cffd31c1a
x-heller <xheller@stuba.sk>
2023-05-16 22:38:13 +0200

    xheller - teacher can upload assignments

--- teacher.php
+++ teacher.php
@@ -18,27 +18,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute();
 
     $selectedFiles = $_POST["selected_files"];
+    $maxPoints = $_POST["max_points"];
+    $fromDates = $_POST["from"];
+    $toDates = $_POST["to"];
 
-    foreach ($selectedFiles as $file) {
+    $isValid = true;
+    foreach ($selectedFiles as $key => $file) {
         $name = pathinfo($file, PATHINFO_FILENAME);
         $format = pathinfo($file, PATHINFO_EXTENSION);
+        $filename = $name . "." . $format;
+        $maxPoint = $maxPoints[$key];
+        $fromDate = !empty($fromDates[$key]) ? $fromDates[$key] : null;
+        $toDate = !empty($toDates[$key]) ? $toDates[$key] : null;
 
-        $filename = $name . "." .$format;
+        if (empty($maxPoint)) {
+            $isValid = false;
+            break;
+        }
 
-        $stmt = $pdo->prepare("INSERT INTO assignments (filename) VALUES (?)");
-        $stmt->execute([$filename]);
+        $stmt = $pdo->prepare("INSERT INTO assignments (filename, point, time_from, time_to) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$filename, $maxPoint, $fromDate, $toDate]);
     }
 
-    // Redirect or display a success message
-    // header("Location: success.php");
-    // echo "Files inserted successfully!";
-    // You can choose to redirect the user to another page or display a success message here
+    if ($isValid) {
 
-    // Redirect to prevent duplicate form submission
-    header("Location: ".$_SERVER['PHP_SELF']);
-    exit();
+        // Redirect to prevent duplicate form submission
+        header("Location: ".$_SERVER['PHP_SELF']);
+        exit();
+    } else {
+        $warningMessage = "Please provide maximum points for all checked assignments.";
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -48,6 +58,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Teacher</title>
 
     <link rel="stylesheet" href="beauty.css">
+    <!--<script>
+        function validateForm() {
+            var checkboxes = document.getElementsByName("selected_files[]");
+            var maxPoints = document.getElementsByName("max_points[]");
+
+            for (var i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].checked && maxPoints[i].value === "") {
+                    alert("Please provide maximum points for all checked assignments.");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    </script>-->
 </head>
 <body>
     <h1 id="title">Teacher portal</h1>
@@ -55,6 +80,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <div id="tableDiv">
         <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+            <?php if (isset($warningMessage)): ?>
+                <div style="background-color: red; color: white;"><?php echo $warningMessage; ?></div>
+            <?php endif; ?>
             <table>
                 <thead>
                 <tr>
@@ -70,11 +98,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <?php foreach ($files as $file): ?>
                     <?php if ($file !== '.' && $file !== '..'): ?>
                         <tr>
-                            <td><input type="checkbox" name="selected_files[]" value="<?php echo $file; ?>"></td>
+                            <?php
+                            $checkboxName = "selected_files[]";
+                            $checkboxValue = $file;
+                            $isChecked = in_array($file, isset($_POST["selected_files"]) ? $_POST["selected_files"] : []);
+                            $maxPointName = "max_points[]";
+                            $maxPointValue = isset($_POST["max_points"][$key]) ? $_POST["max_points"][$key] : "";
+                            $fromName = "from[]";
+                            $fromValue = isset($_POST["from"][$key]) ? $_POST["from"][$key] : "";
+                            $toName = "to[]";
+                            $toValue = isset($_POST["to"][$key]) ? $_POST["to"][$key] : "";
+                            ?>
+                            <td><input type="checkbox" name="<?php echo $checkboxName; ?>" value="<?php echo $checkboxValue; ?>" <?php if ($isChecked) echo 'checked'; ?>></td>
                             <td id="asName"><?php echo pathinfo($file, PATHINFO_FILENAME); ?></td>
-
-                            <td><input type="number" id="max-points" name="max_points" min="0" max="10"></td>
-                            <td><input type="date" id="from" name="from"> <input type="date" id="to" name="to"></td>
+                            <td><input type="number" id="max-points" name="<?php echo $maxPointName; ?>" min="0" max="10" value="<?php echo $maxPointValue; ?>"></td>
+                            <td>
+                                <input type="date" id="from" name="<?php echo $fromName; ?>" value="<?php echo $fromValue; ?>">
+                                <input type="date" id="to" name="<?php echo $toName; ?>" value="<?php echo $toValue; ?>">
+                            </td>
                         </tr>
                     <?php endif; ?>
                 <?php endforeach; ?>
