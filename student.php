3dfad5e6e7fcf054819300d3090f3e8ccc54fd75
xbukris <xbukris@stuba.sk>
2023-05-16 14:50:02 +0200

    xbukris(small fixes and css)

--- student.php
+++ student.php
@@ -1,7 +1,12 @@
 <?php
+//show errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 session_start();
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true && $_SESSION["type"] == "student"){
-    echo "Hello student";
 }
 else{
     header("location: login.php");
@@ -13,11 +18,14 @@ else{
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Student</title>
+    <title>Student portal</title>
+    <link rel="stylesheet" href="beauty.css">
 </head>
 <body>
-<h1>Student</h1>
-<a href="logout.php">logout</a>
-<br>
-<a href="test/asd.php">generate test</a>
+<h1 id="title">Student portal</h1>
+<h3 id="name"><?php echo $_SESSION['fullname']?></h3>
+<div id="buttoncontainer">
+<a id="button" href="logout.php">logout</a>
+<a id="button" href="test/asd.php">generate test</a>
+</div>
 </body>
