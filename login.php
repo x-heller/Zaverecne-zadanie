06666819f4878a4330bfbf90ee74fc87023bc6e4
xmarosi <xmarosi@stuba.sk>
2023-05-18 12:19:35 +0200

    logincss

--- login.php
+++ login.php
@@ -72,61 +72,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!doctype html>
 <html lang="sk">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <title>Login/register s 2FA - Login</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <title>Login/register s 2FA - Login</title>
 
-    <style>
-        html {
-            max-width: 70ch;
-            padding: 3em 1em;
-            margin: auto;
-            line-height: 1.75;
-            font-size: 1.25em;
-        }
+        <style>
+            p,ul,ol {
+                margin-bottom: 2em;
+                color: #1d1d1d;
+            }
 
-        h1,h2,h3,h4,h5,h6 {
-            margin: 1em 0 1em;
-        }
+            html, body {
+                background-color: #f4f4f4;
+                font-family: Calibri, monospace;
 
-        p,ul,ol {
-            margin-bottom: 2em;
-            color: #1d1d1d;
-            font-family: sans-serif;
-        }
-    </style>
-</head>
-<body>
-<header>
-    <hgroup>
-        <h1>Prihlasenie</h1>
-        <h2>Prihlasenie pouzivatela po registracii</h2>
-    </hgroup>
-</header>
-<main>
+            }
 
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            .my-btn {
+                margin-top: 20px;
+            }
+            .my-container {
+                margin: 200px auto;
+                width: 300px;
+                height: 320px;
+                padding: 40px;
+                background-color: rgb(255, 255, 255);
+                border-radius: 10px;
+                box-shadow: 0 1px 2px 1px #ddd;
 
-        <label for="login">
-            Prihlasovacie meno:
-            <input type="text" name="login" value="" id="login" required>
-        </label>
-        <br>
-        <br>
-        <label for="password">
-            Heslo:
-            <input type="password" name="password" value="" id="password" required>
-        </label>
-        <br>
-        <button type="submit">Prihlasit sa</button>
-    </form>
-    <br>
-    <p>Este nemate vytvorene konto?<br><br><a style="box-shadow: 0 1px 2px 1px #ddd;color: #545454;background-color: #ffffff;" class="btn btn-lg btn-google btn-block text-uppercase btn-outline" href="register.php">Registrujte sa tu</a></p>
-</main>
-</body>
+            }
+
+        </style>
+    </head>
+    <body>
+
+        <div class="my-container">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <h1>Prihlasenie</h1>
+                <label for="login">
+                    <input type="text" name="login" value="" id="login" placeholder="Meno" required>
+                </label>
+                <br>
+                <br>
+                <label for="password">
+                    <input type="password" name="password" value="" id="password" placeholder="Heslo" required>
+                </label>
+                <br>
+                <button style="box-shadow: 0 1px 2px 1px #ddd;color: #ffffff;background-color: #3993f0;" class="btn btn-sm btn-google btn-block text-uppercase btn-outline my-btn" type="submit">Prihlasit sa</button>
+            </form>
+            <a style="box-shadow: 0 1px 2px 1px #ddd;color: #ffffff;background-color: #709dd2;" class="btn btn-sm btn-google btn-block text-uppercase btn-outline my-btn" href="register.php">Registrujte sa tu</a>
+        </div>
+
+    </body>
 </html>
